192361284a2e079bc1fe2ebbe72dbbc35a87fdb0
whjin <1414848321@qq.com>
2023-03-26 23:31:04 +0800

    add project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            '@': './src'
+        }
+    }
+};

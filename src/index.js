93085d9e5bff971aaa6eff4b538148be2f18da56
whjin <1414848321@qq.com>
2023-03-27 23:37:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createPolygon, createFiveStar, createCagePalette, createGradientPalette
 
 window.onload = function () {
     const canvasWrapper = document.querySelector(".canvas-wrapper");
+    drawImage(canvasWrapper);
     drawPolygon(canvasWrapper);
     drawFiveStar(canvasWrapper);
     drawCagePalette(canvasWrapper);
@@ -12,19 +13,22 @@ window.onload = function () {
     drawSector(canvasWrapper);
 };
 
-function createEle (canvasWrapper) {
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('width', '200');
-    canvas.setAttribute('height', '150');
-    canvas.style.border = '1px dashed gray';
-    canvasWrapper.appendChild(canvas);
-    return canvas;
+// 绘制图片
+function drawImage (canvasWrapper) {
+    let canvas = createEle(canvasWrapper);
+    canvas.setAttribute('id', 'canvas9');
+    const ctx = canvas.getContext('2d');
+    const image = new Image();
+    image.src = "src/static/images/bvb.png";
+    image.onload = function () {
+        ctx.drawImage(image, 0, 0, 150, 150);
+    };
 }
 
 // 绘制正多边形
 function drawPolygon (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas1');
+    canvas.setAttribute('id', 'canvas8');
     const ctx = canvas.getContext('2d');
     createPolygon(ctx, 3, 100, 75, 50);
     ctx.fillStyle = "HotPink";
@@ -34,7 +38,7 @@ function drawPolygon (canvasWrapper) {
 // 绘制五角星
 function drawFiveStar (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas2');
+    canvas.setAttribute('id', 'canvas7');
     const ctx = canvas.getContext('2d');
     createFiveStar(ctx);
 }
@@ -42,7 +46,7 @@ function drawFiveStar (canvasWrapper) {
 // 绘制方格调色板
 function drawCagePalette (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas3');
+    canvas.setAttribute('id', 'canvas6');
     const ctx = canvas.getContext('2d');
     createCagePalette(ctx);
 }
@@ -50,7 +54,7 @@ function drawCagePalette (canvasWrapper) {
 // 绘制渐变调色板
 function drawGradientPalette (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas4');
+    canvas.setAttribute('id', 'canvas5');
     const ctx = canvas.getContext('2d');
     createGradientPalette(ctx, canvas.height);
 }
@@ -58,7 +62,7 @@ function drawGradientPalette (canvasWrapper) {
 // 绘制圆形
 function drawRound (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas5');
+    canvas.setAttribute('id', 'canvas4');
     const ctx = canvas.getContext('2d');
     // 半圆
     ctx.beginPath();
@@ -77,7 +81,7 @@ function drawRound (canvasWrapper) {
 // 绘制圆角矩形
 function drawRoundedRect (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas5');
+    canvas.setAttribute('id', 'canvas3');
     const ctx = canvas.getContext('2d');
     createRoundedRect(ctx, 100, 100, 20, 20, 20);
     ctx.fillStyle = "HotPink";
@@ -87,7 +91,7 @@ function drawRoundedRect (canvasWrapper) {
 // 绘制N叶草
 function drawLeaf (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas6');
+    canvas.setAttribute('id', 'canvas2');
     const ctx = canvas.getContext('2d');
     createLeaf(ctx, 4, canvas.width / 2, canvas.height / 2, 20, 80);
     ctx.fillStyle = "#00ff99";
@@ -97,9 +101,18 @@ function drawLeaf (canvasWrapper) {
 // 绘制扇形
 function drawSector (canvasWrapper) {
     let canvas = createEle(canvasWrapper);
-    canvas.setAttribute('id', 'canvas7');
+    canvas.setAttribute('id', 'canvas1');
     const ctx = canvas.getContext('2d');
     createSector(ctx, canvas.width / 2, canvas.height / 2, 50, 30, 120);
     ctx.fillStyle = "HotPink";
     ctx.fill();
+}
+
+function createEle (canvasWrapper) {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('width', '200');
+    canvas.setAttribute('height', '150');
+    canvas.style.border = '1px dashed gray';
+    canvasWrapper.appendChild(canvas);
+    return canvas;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: path.resolve(__dirname, "static")
     },
     module: {
         rules: [

f884c2eddb435e2899737038ee5855e679c91552
whjin <1414848321@qq.com>
2023-03-29 14:35:56 +0800

    update

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,24 +1,45 @@
-window.tools = {};
 
-window.tools.getMouse = element => {
-    const mouse = { x: 0, y: 0 };
-    addEvent(element, 'mousemove', (e = window.event) => {
-        let x, y;
-        if (e.pageX || e.pageY) {
-            x = e.pageX;
-            y = pageY;
-        } else {
-            x = e.clientX + document.body.scrollLeft || document.documentElement.scrollLeft;
-            y = e.clientY + document.body.scrollTop || document.documentElement.scrollTop;
-        }
-        x -= element.offsetLeft;
-        y -= element.offsetTop;
-        mouse.x = x;
-        mouse.y = y;
-    });
-    return mouse;
-};
+export class Arrow {
+    constructor (x = 0, y = 0, color = '#ff0099', angle = 0) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.angle = angle;
+    };
 
-window.tools.getKey=()=>{
-    
+    stroke (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        ctx.moveTo(-20, -10);
+        ctx.lineTo(0, -10);
+        ctx.lineTo(0, -20);
+        ctx.lineTo(20, 0);
+        ctx.lineTo(0, 20);
+        ctx.lineTo(0, 10);
+        ctx.lineTo(-20, 10);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    fill (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        ctx.moveTo(-20, -10);
+        ctx.lineTo(0, -10);
+        ctx.lineTo(0, -20);
+        ctx.lineTo(20, 0);
+        ctx.lineTo(0, 20);
+        ctx.lineTo(0, 10);
+        ctx.lineTo(-20, 10);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    }
 }
